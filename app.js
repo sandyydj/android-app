d7d197c5c4367d40f519b2ed92324b852b13d442
Tim Schindler <tim.schindler@adcloud.com>
2012-11-29 14:33:42 +0100

    added localstorage support and hope the app uses it

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
+var userType = localStorage.getItem("userType") || ""
+  , shortTerm = localStorage.getItem("shortTerm") || ""
+
+
+
+function isValid() {
+  return userType === "" || shortTerm === "";
+}
+
+
+
+function renderPlan() {
+  if (isValid()) {
+    return;
+  }
+
+  $("span.userType").text(userType === "t" ? "Lehrer" : "Schueler");
+  $("span.shortTerm").text(window[userType + "Hash"][shortTerm]);
+  $("div#shortTermSelectBoxHtml select").html(getOptionsHtml())
+
+  var url = "http://iskariot.uphero.com/index.php?art=" +
+    userType + "&nummer=" + userType + getPlaceholder(shortTerm) + shortTerm;
+
+  $("#iframeId").attr("src", url);
+}
+
+
+
+function getPlaceholder(shortTerm) {
+  var placeholder = "00";
+
+  if (shortTerm.length === 1) {
+    placeholder += "00";
+  }
+
+  if (shortTerm.length === 2) {
+    placeholder += "0";
+  }
+
+  return placeholder;
+}
+
+
+
+function getOptionsHtml() {
+  var html = "";
+
+  $.each(window[userType + "Hash"], function (value, key) {
+    html += '<option value="' + value + '">' + key + '</option>';
+  });
+  console.log(html);
+  return html;
+}
+
+
+
 $(document).ready(function () {
-  var userType = ""
-    , shortTerm = ""
-
-
+  renderPlan();
 
   // user type selection
   $(".userType").change(function () {
     userType = $(".userType option:selected").val()
-    $("div#shortTermSelectBoxHtml select").html(window[userType + "SelectBoxHtml"])
+    localStorage.setItem("userType", userType);
+
+    $("div#shortTermSelectBoxHtml select").html(getOptionsHtml())
     $("span.shortTerm").text("Kuerzel waehlen")
   });
 
@@ -16,31 +71,8 @@ $(document).ready(function () {
   // short term selection
   $(".shortTerm").change(function () {
     shortTerm = $(".shortTerm option:selected").val()
+    localStorage.setItem("shortTerm", shortTerm);
 
-    if (userType === "" || shortTerm === "") {
-      return;
-    }
-
-    var url = "http://iskariot.uphero.com/index.php?art=" +
-      userType + "&nummer=" + userType + getPlaceholder(shortTerm) + shortTerm;
-
-    $("#iframeId").attr("src", url);
+    renderPlan()
   });
-
-
-
-  // helper function to prepare required placeholders
-  function getPlaceholder(shortTerm) {
-    var placeholder = "00";
-
-    if (shortTerm.length === 1) {
-      placeholder += "00";
-    }
-
-    if (shortTerm.length === 2) {
-      placeholder += "0";
-    }
-
-    return placeholder;
-  }
 });
--- selectboxes.js
+++ selectboxes.js
@@ -1,199 +1,199 @@
-var tSelectBoxHtml = '' +
-  '<option value="">Kuerzel waehlen</option>' +
-  '<option value="1">AL</option>' +
-  '<option value="2">AM</option>' +
-  '<option value="3">BE</option>' +
-  '<option value="4">BH</option>' +
-  '<option value="5">BL</option>' +
-  '<option value="6">BN</option>' +
-  '<option value="7">BO</option>' +
-  '<option value="8">DH</option>' +
-  '<option value="9">DI</option>' +
-  '<option value="10">DL</option>' +
-  '<option value="11">DS</option>' +
-  '<option value="12">FG</option>' +
-  '<option value="13">FK</option>' +
-  '<option value="14">FN</option>' +
-  '<option value="15">FR</option>' +
-  '<option value="16">FZ</option>' +
-  '<option value="17">GA</option>' +
-  '<option value="18">GH</option>' +
-  '<option value="19">GN</option>' +
-  '<option value="20">GO</option>' +
-  '<option value="21">GR</option>' +
-  '<option value="22">GV</option>' +
-  '<option value="23">HA</option>' +
-  '<option value="24">HL</option>' +
-  '<option value="25">HX</option>' +
-  '<option value="26">KA</option>' +
-  '<option value="27">KE</option>' +
-  '<option value="28">KF</option>' +
-  '<option value="29">KH</option>' +
-  '<option value="30">KI</option>' +
-  '<option value="31">KL</option>' +
-  '<option value="32">KP</option>' +
-  '<option value="33">KR</option>' +
-  '<option value="34">KS</option>' +
-  '<option value="35">KT</option>' +
-  '<option value="36">LA</option>' +
-  '<option value="37">LD</option>' +
-  '<option value="38">LG</option>' +
-  '<option value="39">LN</option>' +
-  '<option value="40">LR</option>' +
-  '<option value="41">MC</option>' +
-  '<option value="42">ME</option>' +
-  '<option value="43">MF</option>' +
-  '<option value="44">MM</option>' +
-  '<option value="45">MN</option>' +
-  '<option value="46">MR</option>' +
-  '<option value="47">MS</option>' +
-  '<option value="48">MY</option>' +
-  '<option value="49">NH</option>' +
-  '<option value="50">NI</option>' +
-  '<option value="51">NL</option>' +
-  '<option value="52">NR</option>' +
-  '<option value="53">NT</option>' +
-  '<option value="54">OR</option>' +
-  '<option value="55">OU</option>' +
-  '<option value="56">PL</option>' +
-  '<option value="57">PN</option>' +
-  '<option value="58">PO</option>' +
-  '<option value="59">RA</option>' +
-  '<option value="60">RR</option>' +
-  '<option value="61">RS</option>' +
-  '<option value="62">RU</option>' +
-  '<option value="63">RY</option>' +
-  '<option value="64">RZ</option>' +
-  '<option value="65">SA</option>' +
-  '<option value="66">SI</option>' +
-  '<option value="67">SM</option>' +
-  '<option value="68">SN</option>' +
-  '<option value="69">SR</option>' +
-  '<option value="70">ST</option>' +
-  '<option value="71">TG</option>' +
-  '<option value="72">TN</option>' +
-  '<option value="73">TS</option>' +
-  '<option value="74">UL</option>' +
-  '<option value="75">VH</option>' +
-  '<option value="76">WD</option>' +
-  '<option value="77">WE</option>' +
-  '<option value="78">WF</option>' +
-  '<option value="79">WK</option>' +
-  '<option value="80">WR</option>' +
-  '<option value="81">WU</option>' +
-  '<option value="82">WZ</option>';
+var tHash = {
+  "1": "AL",
+  "2": "AM",
+  "3": "BE",
+  "4": "BH",
+  "5": "BL",
+  "6": "BN",
+  "7": "BO",
+  "8": "DH",
+  "9": "DI",
+  "10": "DL",
+  "11": "DS",
+  "12": "FG",
+  "13": "FK",
+  "14": "FN",
+  "15": "FR",
+  "16": "FZ",
+  "17": "GA",
+  "18": "GH",
+  "19": "GN",
+  "20": "GO",
+  "21": "GR",
+  "22": "GV",
+  "23": "HA",
+  "24": "HL",
+  "25": "HX",
+  "26": "KA",
+  "27": "KE",
+  "28": "KF",
+  "29": "KH",
+  "30": "KI",
+  "31": "KL",
+  "32": "KP",
+  "33": "KR",
+  "34": "KS",
+  "35": "KT",
+  "36": "LA",
+  "37": "LD",
+  "38": "LG",
+  "39": "LN",
+  "40": "LR",
+  "41": "MC",
+  "42": "ME",
+  "43": "MF",
+  "44": "MM",
+  "45": "MN",
+  "46": "MR",
+  "47": "MS",
+  "48": "MY",
+  "49": "NH",
+  "50": "NI",
+  "51": "NL",
+  "52": "NR",
+  "53": "NT",
+  "54": "OR",
+  "55": "OU",
+  "56": "PL",
+  "57": "PN",
+  "58": "PO",
+  "59": "RA",
+  "60": "RR",
+  "61": "RS",
+  "62": "RU",
+  "63": "RY",
+  "64": "RZ",
+  "65": "SA",
+  "66": "SI",
+  "67": "SM",
+  "68": "SN",
+  "69": "SR",
+  "70": "ST",
+  "71": "TG",
+  "72": "TN",
+  "73": "TS",
+  "74": "UL",
+  "75": "VH",
+  "76": "WD",
+  "77": "WE",
+  "78": "WF",
+  "79": "WK",
+  "80": "WR",
+  "81": "WU",
+  "82": "WZ"
+}
 
-var cSelectBoxHtml = '' +
-  '<option value="">Kuerzel waehlen</option>' +
-  '<option value="1">AVM01</option>' +
-  '<option value="2">AVM02</option>' +
-  '<option value="3">AVM03</option>' +
-  '<option value="4">AVM04</option>' +
-  '<option value="5">AVM05</option>' +
-  '<option value="6">AVM06</option>' +
-  '<option value="7">AVM11</option>' +
-  '<option value="8">AVM12</option>' +
-  '<option value="9">AVM13</option>' +
-  '<option value="10">AVM14</option>' +
-  '<option value="11">AVM15</option>' +
-  '<option value="12">AVM16</option>' +
-  '<option value="13">AVM21</option>' +
-  '<option value="14">AVM22</option>' +
-  '<option value="15">AVM23</option>' +
-  '<option value="16">AVM24</option>' +
-  '<option value="17">AVM25</option>' +
-  '<option value="18">BFG11</option>' +
-  '<option value="19">BFG21</option>' +
-  '<option value="20">BFT11</option>' +
-  '<option value="21">BFT12</option>' +
-  '<option value="22">BFT13</option>' +
-  '<option value="23">BFT21</option>' +
-  '<option value="24">BFT22</option>' +
-  '<option value="25">BFT23</option>' +
-  '<option value="26">EIB0A</option>' +
-  '<option value="27">EIB1A</option>' +
-  '<option value="28">EIB2A</option>' +
-  '<option value="29">EIB9A</option>' +
-  '<option value="30">EIG0A</option>' +
-  '<option value="31">EIG1A</option>' +
-  '<option value="32">EIG2A</option>' +
-  '<option value="33">EIG2B</option>' +
-  '<option value="34">EIG9A</option>' +
-  '<option value="35">EIG9B</option>' +
-  '<option value="36">FIA01</option>' +
-  '<option value="37">FIA02</option>' +
-  '<option value="38">FIA03</option>' +
-  '<option value="39">FIA0A</option>' +
-  '<option value="40">FIA0B</option>' +
-  '<option value="41">FIA11</option>' +
-  '<option value="42">FIA12</option>' +
-  '<option value="43">FIA13</option>' +
-  '<option value="44">FIA1A</option>' +
-  '<option value="45">FIA1B</option>' +
-  '<option value="46">FIA21</option>' +
-  '<option value="47">FIA22</option>' +
-  '<option value="48">FIA23</option>' +
-  '<option value="49">FIA2A</option>' +
-  '<option value="50">FIA2B</option>' +
-  '<option value="51">FIS01</option>' +
-  '<option value="52">FIS02</option>' +
-  '<option value="53">FIS03</option>' +
-  '<option value="54">FIS04</option>' +
-  '<option value="55">FIS0A</option>' +
-  '<option value="56">FIS0B</option>' +
-  '<option value="57">FIS11</option>' +
-  '<option value="58">FIS12</option>' +
-  '<option value="59">FIS13</option>' +
-  '<option value="60">FIS14</option>' +
-  '<option value="61">FIS15</option>' +
-  '<option value="62">FIS1A</option>' +
-  '<option value="63">FIS1B</option>' +
-  '<option value="64">FIS21</option>' +
-  '<option value="65">FIS22</option>' +
-  '<option value="66">FIS23</option>' +
-  '<option value="67">FIS24</option>' +
-  '<option value="68">FIS25</option>' +
-  '<option value="69">FIS2A</option>' +
-  '<option value="70">FIS2B</option>' +
-  '<option value="71">FOT11</option>' +
-  '<option value="72">FST11</option>' +
-  '<option value="73">FST21</option>' +
-  '<option value="74">FST91</option>' +
-  '<option value="75">IAF01</option>' +
-  '<option value="76">IAF02</option>' +
-  '<option value="77">IAF11</option>' +
-  '<option value="78">IAF12</option>' +
-  '<option value="79">IAF13</option>' +
-  '<option value="80">IAF21</option>' +
-  '<option value="81">IAF22</option>' +
-  '<option value="82">IAF23</option>' +
-  '<option value="83">IAH01</option>' +
-  '<option value="84">IAH11</option>' +
-  '<option value="85">IAH21</option>' +
-  '<option value="86">ITS01</option>' +
-  '<option value="87">ITS05</option>' +
-  '<option value="88">ITS0A</option>' +
-  '<option value="89">ITS11</option>' +
-  '<option value="90">ITS12</option>' +
-  '<option value="91">ITS1A</option>' +
-  '<option value="92">ITS21</option>' +
-  '<option value="93">ITS22</option>' +
-  '<option value="94">ITS2A</option>' +
-  '<option value="95">VTF01</option>' +
-  '<option value="96">VTF02</option>' +
-  '<option value="97">VTF03</option>' +
-  '<option value="98">VTF04</option>' +
-  '<option value="99">VTF05</option>' +
-  '<option value="100">VTF06</option>' +
-  '<option value="101">VTF11</option>' +
-  '<option value="102">VTF12</option>' +
-  '<option value="103">VTF13</option>' +
-  '<option value="104">VTF14</option>' +
-  '<option value="105">VTF15</option>' +
-  '<option value="106">VTF16</option>' +
-  '<option value="107">VTF21</option>' +
-  '<option value="108">VTF22</option>' +
-  '<option value="109">VTF23</option>' +
-  '<option value="110">VTF24</option>' +
-  '<option value="111">VTF25</option>' +
-  '<option value="112">VTF26</option>';
+var cHash = {
+  "1": "AVM01",
+  "2": "AVM02",
+  "3": "AVM03",
+  "4": "AVM04",
+  "5": "AVM05",
+  "6": "AVM06",
+  "7": "AVM11",
+  "8": "AVM12",
+  "9": "AVM13",
+  "10": "AVM14",
+  "11": "AVM15",
+  "12": "AVM16",
+  "13": "AVM21",
+  "14": "AVM22",
+  "15": "AVM23",
+  "16": "AVM24",
+  "17": "AVM25",
+  "18": "BFG11",
+  "19": "BFG21",
+  "20": "BFT11",
+  "21": "BFT12",
+  "22": "BFT13",
+  "23": "BFT21",
+  "24": "BFT22",
+  "25": "BFT23",
+  "26": "EIB0A",
+  "27": "EIB1A",
+  "28": "EIB2A",
+  "29": "EIB9A",
+  "30": "EIG0A",
+  "31": "EIG1A",
+  "32": "EIG2A",
+  "33": "EIG2B",
+  "34": "EIG9A",
+  "35": "EIG9B",
+  "36": "FIA01",
+  "37": "FIA02",
+  "38": "FIA03",
+  "39": "FIA0A",
+  "40": "FIA0B",
+  "41": "FIA11",
+  "42": "FIA12",
+  "43": "FIA13",
+  "44": "FIA1A",
+  "45": "FIA1B",
+  "46": "FIA21",
+  "47": "FIA22",
+  "48": "FIA23",
+  "49": "FIA2A",
+  "50": "FIA2B",
+  "51": "FIS01",
+  "52": "FIS02",
+  "53": "FIS03",
+  "54": "FIS04",
+  "55": "FIS0A",
+  "56": "FIS0B",
+  "57": "FIS11",
+  "58": "FIS12",
+  "59": "FIS13",
+  "60": "FIS14",
+  "61": "FIS15",
+  "62": "FIS1A",
+  "63": "FIS1B",
+  "64": "FIS21",
+  "65": "FIS22",
+  "66": "FIS23",
+  "67": "FIS24",
+  "68": "FIS25",
+  "69": "FIS2A",
+  "70": "FIS2B",
+  "71": "FOT11",
+  "72": "FST11",
+  "73": "FST21",
+  "74": "FST91",
+  "75": "IAF01",
+  "76": "IAF02",
+  "77": "IAF11",
+  "78": "IAF12",
+  "79": "IAF13",
+  "80": "IAF21",
+  "81": "IAF22",
+  "82": "IAF23",
+  "83": "IAH01",
+  "84": "IAH11",
+  "85": "IAH21",
+  "86": "ITS01",
+  "87": "ITS05",
+  "88": "ITS0A",
+  "89": "ITS11",
+  "90": "ITS12",
+  "91": "ITS1A",
+  "92": "ITS21",
+  "93": "ITS22",
+  "94": "ITS2A",
+  "95": "VTF01",
+  "96": "VTF02",
+  "97": "VTF03",
+  "98": "VTF04",
+  "99": "VTF05",
+  "100": "VTF06",
+  "101": "VTF11",
+  "102": "VTF12",
+  "103": "VTF13",
+  "104": "VTF14",
+  "105": "VTF15",
+  "106": "VTF16",
+  "107": "VTF21",
+  "108": "VTF22",
+  "109": "VTF23",
+  "110": "VTF24",
+  "111": "VTF25",
+  "112": "VTF26"
+}
