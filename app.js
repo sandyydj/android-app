e693982d7ad25e0113b785df6c705d3f8dafa0d1
Tim Schindler <tim.schindler@adcloud.com>
2012-11-29 12:44:13 +0100

    made it work

--- app.js
+++ app.js
@@ -1,2 +1,46 @@
+$(document).ready(function () {
+  var userType = ""
+    , shortTerm = ""
 
-console.log($("table"));
+
+
+  // user type selection
+  $(".userType").change(function () {
+    userType = $(".userType option:selected").val()
+    $("div#shortTermSelectBoxHtml select").html(window[userType + "SelectBoxHtml"])
+    $("span.shortTerm").text("Kuerzel waehlen")
+  });
+
+
+
+  // short term selection
+  $(".shortTerm").change(function () {
+    shortTerm = $(".shortTerm option:selected").val()
+
+    if (userType === "" || shortTerm === "") {
+      return;
+    }
+
+    var url = "http://iskariot.uphero.com/index.php?art=" +
+      userType + "&nummer=" + userType + getPlaceholder(shortTerm) + shortTerm;
+
+    $("#iframeId").attr("src", url);
+  });
+
+
+
+  // helper function to prepare required placeholders
+  function getPlaceholder(shortTerm) {
+    var placeholder = "00";
+
+    if (shortTerm.length === 1) {
+      placeholder += "00";
+    }
+
+    if (shortTerm.length === 2) {
+      placeholder += "0";
+    }
+
+    return placeholder;
+  }
+});
